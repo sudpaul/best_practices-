04c13d0e4bf3cfe253773074c85f5bccef3ee7cb
Sudipta Paul <z3525552@ad.unsw.edu.au>
2018-09-23 19:26:44 +1000

    Write docstring for data download, read and quality function

--- data_analysis.py
+++ data_analysis.py
@@ -4,16 +4,29 @@ Created on Fri Sep 21 13:51:23 2018
 
 @author: z3525552
 """
-import requests
 import os
+import requests
 import pandas as pd
 
 #Dowload data
 
 def download_data(base_url, filename, path='data'):
     
-   
+    """This function download a file from given url and
+       write to a file in a local data directory.
     
+    Parameters
+    ----------
+    base_url : str 
+               URL for the download file
+    filename : str
+               Filename
+    path  : str
+            Local directory to save in the disk. Default is data
+    Returns
+    ----------
+     None 
+    """
     
     if not os.path.exists(path):
         os.mkdir(path)
@@ -34,6 +47,19 @@ def download_data(base_url, filename, path='data'):
 
 def read_data(filename):
     
+    """This function read data from local disk
+       and return pandas dataframe object.
+    
+    Parameters
+    ----------
+    filename : str 
+               file name or filepath with filename 
+
+    Returns
+    ----------
+    dataframe : obj
+               pandas dataframe object
+     """
     
     filename = filename.lower()
     filetype = filename.split('.')[-1]
@@ -50,6 +76,12 @@ def read_data(filename):
 
 def data_quality(dataframe):
         
+    """Basice data quality check routine. Input is a pandas dataframe
+    print out the number of columns, name, unique values in and datatypes of the
+    column"""
+    
+    print('Number of Columns the dataset has {}'. format(dataframe.shape[1]))
+    
     for column in dataframe.columns:
         print("Column {} has {} unique values datatype {}".format(
             dataframe[column].name, dataframe[column].nunique(), dataframe[column].dtype
