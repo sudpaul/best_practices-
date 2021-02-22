067480df4d0afedd8fd7640529841ab452473fb0
Sudipta Paul <sudipta116@gmail.com>
2021-02-22 15:03:45 +1100

    Add inline comments

--- csv_files_read.py
+++ csv_files_read.py
@@ -11,11 +11,15 @@ import pandas as pd
 directory_name = ''
 file_type = 'csv'
 output_file = ''
+# Changing the file directory
 os.chdir(directory_name)
+# Joining all same file to a filenames list
 filenames = [f for f in glob.glob('*.{}'.format(file_type))]
-
+# Combaine all csv data to a pandas dataframe
 data = pd.concat([pd.read_csv(name) for name in filenames ])
-
+# Writing the data file to a excel file
 writer = pd.ExcelWriter(directory_name+'/'+ output_file)
-data.to_excel(writer,sheet_name=sheetname, index=False, encoding='utf-8')
+
+data.to_excel(writer, index=False, encoding='utf-8')
+
 writer.save()
