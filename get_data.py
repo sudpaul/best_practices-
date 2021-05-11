72cc2989118e53e7fd52be05eba5f83500ce7591
Sudipta Paul <sudipta116@gmail.com>
2021-05-11 17:38:45 +1000

    Generic script to get data from a directory

---
+++ get_data.py
@@ -0,0 +1,10 @@
+import os
+import glob
+def get_files(filepath):
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+    
+    return all_files
