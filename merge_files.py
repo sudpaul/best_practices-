c725d32834b001ca512d9f64be77053c2b2445b0
Sudipta Paul <sudipta116@gmail.com>
2022-03-08 12:20:06 +1100

    Update merge_files.py
    Change from absolute path to relative path

--- merge_files.py
+++ merge_files.py
@@ -7,10 +7,10 @@ Created on Wed Nov 11 11:36:49 2020
 import os
 import glob
 import pandas as pd
-os.chdir("C:/Users/z3525552/BORIS_import")
+os.chdir(filepath)
 file_type = 'csv'
-
-
+filepath = '~/myfile/raw-data'
+clean_data = '~/myfile/clean-data'
 def load_data(file):
     df = pd.read_csv(file,skiprows=1)
     return df
@@ -20,6 +20,6 @@ filenames = [f for f in glob.glob('*.{}'.format(file_type))]
 data = pd.concat([load_data(name) for name in filenames ])
 
 
-writer = pd.ExcelWriter("C:/Users/z3525552/BORIS_import/Medicine_Clinical_school.xlsx")
+writer = pd.ExcelWriter(clean_data)
 data.to_excel(writer,'schools', index=False, encoding='utf-8')
 writer.save()
